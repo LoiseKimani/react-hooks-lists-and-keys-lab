10d43585c5445b4257685d14dbbdda195453246f
Loise <loise.kimani@student.moringaschool.com>
2023-01-16 07:26:11 +0300

    Assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,37 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList({ props }) {
+  console.log(props);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {props.map(project => 
+        <ProjectItem key={props.id}
+          name={props.name} 
+          about={props.about} 
+          technologies={props.technologies}/>)}
+      </div>
     </div>
   );
+
 }
 
 export default ProjectList;
+
+
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">{/* render ProjectItem components here */}</div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
